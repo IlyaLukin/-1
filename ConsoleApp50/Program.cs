069a611e08529f4413f6c13bfcbde7bd6b121d7e
Лукин Илья Павлович <iplukin@edu.hse.ru>
2019-06-12 20:13:00 +0500

    Added

--- ConsoleApp50/Program.cs
+++ ConsoleApp50/Program.cs
@@ -7,53 +7,73 @@ using System.Collections;
 
 namespace ConsoleApp50
 {
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            int n=0, step=0;
-            int[] a = new int[0];
-            for(int i = 0; i < n; i++)
+            Console.WriteLine("Билетики");
+            int n, k, ans, all;
+            string s;
+            bool flag;
+            string[] split;
+            Console.WriteLine("\nВведите два числа N (1<=N<=500000) и К (1<=К<=1000) (K<=N) через пробел: ");
+            s = Console.ReadLine();
+            split = s.Split(new char[] { ' ' });
+            int.TryParse(split[0], out n);
+            int.TryParse(split[1], out k);
+            Console.WriteLine("Введите число состоящее из 0 и 1 через пробел: ");
+            s = Console.ReadLine();
+            split = s.Split(new char[] { ' ' });
+            int[] x = new int[n];
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(a[i]);
+                int.TryParse(split[i], out x[i]);
             }
-
-            const int UNDEF = -1;
-            int changePos = UNDEF;
-            for (int start = 0; start < step; start++)
+            ans = 0;
+            flag = false;
+            int z = 0;
+            for (int i = 0; i < n - k; i++)
             {
-                int n0 = 0;
-                int n1 = 0;
-                int pos0 = UNDEF;
-                int pos1 = UNDEF;
-                for(int i = start; i < n; i += step)
+                if (x[i] != x[i + k]) z += 1;
+            }
+            ans = -1;
+            if (z == 0) ans = 0;
+            else
+            {
+                for (int i = 0; i < n; i++)
                 {
-                    if(a[i]==0)
+                    all = 0;
+                    if ((i - k) >= 0)
                     {
-                        n0++;
-                        if (n0 == 1)
-                        {
-                            pos0 = i;
-                        }
-                    } else if (a[i] == 1)
-                    {
-                        n1++;
-                        if (n1 == 1)
-                        {
-                            pos1 = i;
-                        }
+                        if (x[i] == x[i - k]) all++;
+                        else all--;
                     }
-                    
+                    if ((i + k) < n)
+                    {
+                        if (x[i] == x[i + k]) all++;
+                        else all--;
+                    }
+                    if ((all + z) == 0)
+                    {
+                        ans = i + 1;
+                        break;
+                    }
                 }
-
-                if(n0==0||n1==0)
-                { continue; }
-                if (n0 > 1 && n1 > 1||changePos!=UNDEF) { Console.WriteLine("FAIL"); /*return 0;*/ }
-                if (n0 == 1 && n1 == 1) { changePos=(pos0,pos1)}
             }
-            //return 0;
-
+            if (ans == -1)
+            {
+                Console.WriteLine("FAIL");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+                Console.WriteLine($"{ans}");
+            }
+            Console.ReadKey();
         }
     }
+
 }
+    
+
